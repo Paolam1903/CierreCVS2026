b3ad4750cdc10498cc2e8fcdd96e8cd5d2b79584
Paolam1903 <sernapaola922@gmail.com>
2026-02-04 16:29:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,9 +21,7 @@ LOGO_PATH = BASE_DIR / "logo.png"
 
 RUTA_LIQ = DATA_DIR / "liquidacion_final.xlsx"
 RUTA_METAS = DATA_DIR / "metas.xlsx"
-st.write("Ruta data:", DATA_DIR)
-st.write("Existe liquidación:", RUTA_LIQ.exists())
-st.write("Existe metas:", RUTA_METAS.exists())
+
 # =============================
 # VALIDACIÓN
 # =============================
@@ -401,4 +399,5 @@ with tab2:
                 open("Historico_Comisiones.xlsx", "rb"),
                 file_name="Historico_Comisiones.xlsx"
             )
+
 
