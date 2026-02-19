c309db83defc0c2bf09a706ca3a67dd1ff1cea72
Paolam1903 <sernapaola922@gmail.com>
2026-02-19 14:55:26 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -426,6 +426,7 @@ def construir_tabla_productos(df_vendedor, maestro, df_cvs, rol):
     # =====================
     # VENTAS POR PRODUCTO
     # =====================
+
     ejec = (
         df_vendedor.groupby("Producto")["Cantidad"]
         .sum()
@@ -438,17 +439,18 @@ def construir_tabla_productos(df_vendedor, maestro, df_cvs, rol):
 
         # aplicar distribución
         meta_ajustada = meta * porcentaje
+        meta_final = int(round(meta_ajustada))  # redondeo UNA sola vez
 
         ejecutado = ejec.get(producto, 0)
 
-        if meta_ajustada > 0:
-            pct = int(round((ejecutado / meta_ajustada) * 100))
+        if meta_final > 0:
+            pct = int(round((ejecutado / meta_final) * 100))
         else:
             pct = 0
 
         filas.append({
             "Producto": producto,
-            "Meta_Producto": int(round(meta_ajustada)),
+            "Meta_Producto": meta_final,
             "Ejecutado": int(ejecutado),
             "% Cumplimiento": f"{pct}%"
         })
@@ -456,8 +458,6 @@ def construir_tabla_productos(df_vendedor, maestro, df_cvs, rol):
     tabla = pd.DataFrame(filas)
 
     return tabla
-
-
 
 
 # =====================
@@ -596,7 +596,7 @@ with tab2:
                 and x["Nombre"] == nombre_lider
                 and x["Producto"] == r["Producto"]
             ),
-            ("Pago 100%", "")
+            ("Sin pago (0%)", "")
         ),
         axis=1,
         result_type="expand"
@@ -692,7 +692,7 @@ with tab2:
                         and x["Nombre"] == nombre
                         and x["Producto"] == r["Producto"]
                     ),
-                    ("Pago 100%", "")
+                    ("Sin pago (0%)", "")
                 ),
                 axis=1,
                 result_type="expand"
